ac6e18d453e400237c71df78c19587331f5169d4
Rebeca Alves Lima <cc21448@g.unicamp.com>
2022-12-04 20:16:28 -0300

    adicionando botoes e interação com sql

--- src/components/CrudCarro/CrudCarro.js
+++ src/components/CrudCarro/CrudCarro.js
@@ -1,32 +1,127 @@
 import React, {Component} from "react";
+import axios from "axios";
 import './CrudCarro.css';
 import Main from "../template/Main";
 
 const title = "Carros";
 
-const Carros =[
+const urlApi = "http://localhost:5174/api/carro"
+const initialState= {
+    carro: {id:0, ano:'', modelo:'', estatus:''},
+    lista:[]
+}
+/*const Carros =[
     {'id': 1, 'placa': 1111, 'modelo': 'Siena', 'ano': 2012},
     {'id': 2, 'placa': 2222, 'modelo': 'Corsa', 'ano': 2013},
 
-]
+]*/
 
 export default class CrudCarro extends Component{
+    state={...initialState}
+    componentDidMount(){
+        axios(urlApi).then(resp =>{
+            this.setState({lista:resp.data})
+        })
+    }
+    limpar(){
+        this.setState({carro:initialState.carro})
+    }
+    salvar(){
+        const carro = this.state.carro;
+        carro.ano = Number(carro.ano);
+        const metodo = carro.id ? 'put' : 'post';
+        const url = carro.id ? `${urlApi}/${carro.id}` : urlApi;
+
+        axios[metodo](urlApi, carro).then(resp =>{
+            const lista = this.getListaAtualizada(resp.data)
+            this.setState({carro: initialState.carro,lista})
+        })
+    }
+    getListaAtualizada(carro, add=true){
+        const lista  = this.state.lista.filter(a => a.id !== carro.id);
+        if (add) lista.unshift(carro);
+        return lista;
+    }
+    atualizaCampo(event){
+        const carro = {...this.state.carro};
+        carro[event.target.name] = event.target.value;
+        this.setState({carro})
+    }
+    carregar(carro){
+        this.setState({carro})
+    }
+    remover(carro){
+        const url = urlApi + "/" +carro.id;
+        if(window.confirm("Confirmar remoção do aluno: " + carro.placa)){
+            console.log("entrou no confirm");
+            axios['delete'](url, aluno).then(resp =>{
+                const lista = this.getListaAtualizada(carro,false)
+                this.setState({carro: initialState.carro, lista})
+            })
+        }
+    }
+    renderForm(){
+        return(
+            <div className="inclui-container">
+                <label>Ano</label>
+                <input type="text"
+                        id="ano"
+                        placeholder="Ano do carro"
+                        className="form-input"
+                        name="ano"
+                        value={this.state.carro.ano}
+                        onChange={e=> this.atualizaCampo(e)} />
+                <label>Modelo</label>
+                <input type="text"
+                        id="modelo"
+                        placeholder="Modelo do carro"
+                        className="form-input"
+                        name="modelo"
+                        value={this.state.carro.modelo}
+                        onChange={e => this.atualizaCampo(e)} />
+                <label>Estatus</label>
+                <input type="text"
+                        id="estatus"
+                        placeholder="Estatus do carro"
+                        className="form-input"
+                        name="estatus"
+                        value={this.state.carro.estatus}
+                        onChange={e => this.atualizaCampo(e)} />
+                <button className="btnSalvar"
+                        onClick={e => this.salvar(e)}>
+                            Salvar</button>
+                <button className="btnCancelar"
+                        onClick={e =>this.limpar(e)}>
+                            Cancelar</button>
+            </div>
+        )
+    }
     renderTable(){
         return(
             <div className="lista">
                 <table className="listaCarros" id="tblistaCarros">
                     <thead className="tabela">
-                        <th className="tabTituloPlaca">Placa</th>
-                        <th className="tabTituloModelo">Modelo</th>
                         <th className="tabTituloAno">Ano</th>
+                        <th className="tabTituloModelo">Modelo</th>
+                        <th className="tabTituloEstatus">estatus</th>
                     </thead>
                     <tbody>
-                        {Carros.map(
+                        {this.state.map(
                             (carro)=>
                             <tr key={carro.id}>
-                                <td>{carro.placa}</td>
-                                <td>{carro.modelo}</td>
                                 <td>{carro.ano}</td>
+                                <td>{carro.modelo}</td>
+                                <td>{carro.estatus}</td>
+                                <td>
+                                    <button onClick={()=>this.carregar(carro)}>
+                                        Alterar
+                                    </button>
+                                </td>
+                                <td>
+                                    <button onClick={()=>this.remover(carro)}>
+                                        Remover
+                                    </button>
+                                </td>
                             </tr>
                         )}
                     </tbody>
@@ -37,6 +132,7 @@ export default class CrudCarro extends Component{
     render(){
         return(
             <Main title={title}>
+                {this.renderForm()}
                 {this.renderTable()}
             </Main>
         )
